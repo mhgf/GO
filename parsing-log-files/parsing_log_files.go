e0af9d11908c141d72933b5a9c2653875878664b
Matheus Ferreira <mhferreira.14@outlook.com>
2024-01-08 23:30:04 -0300

    init

---
+++ parsing-log-files/parsing_log_files.go
@@ -0,0 +1,45 @@
+package parsinglogfiles
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func IsValidLine(text string) bool {
+	re := regexp.MustCompile(`^\[(TRC|DBG|INF|WRN|ERR|FTL)]`)
+	return re.MatchString(text)
+}
+
+func SplitLogLine(text string) []string {
+	re := regexp.MustCompile(`<[-=~*]*>`)
+	return re.Split(text, -1)
+}
+
+func CountQuotedPasswords(lines []string) int {
+	re := regexp.MustCompile(`(?i)".*password.*"`)
+	total := 0
+	for _, line := range lines {
+		if re.MatchString(line) {
+			total++
+		}
+	}
+	return total
+}
+
+func RemoveEndOfLineText(text string) string {
+	re := regexp.MustCompile(`end-of-line\d*`)
+	return re.ReplaceAllString(text, "")
+}
+
+func TagWithUserName(lines []string) []string {
+	re := regexp.MustCompile(`User\s+(\w+)`)
+	newArr := []string{}
+	for _, line := range lines {
+		founds := re.FindStringSubmatch(line)
+		if founds != nil {
+			line = fmt.Sprintf("[USR] %s %s", founds[1], line)
+		}
+		newArr = append(newArr, line)
+	}
+	return newArr
+}
